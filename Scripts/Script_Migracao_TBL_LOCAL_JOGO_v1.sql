b2cc531b9b10ea108a6fb32aaa8433b9e59ce9df
Matheus <matheuswpolo23@gmail.com>
2020-04-21 16:26:34 -0300

    migração de informações de ponto de encrontro e locais de jogos

---
+++ Scripts/Script_Migracao_TBL_LOCAL_JOGO_v1.sql
@@ -0,0 +1,35 @@
+-- SCRIPT MIGRAÇÃO DE LOCAIS DE JOGOS 
+
+USE CAOFRADES_FC
+
+INSERT INTO TBL_LOCAL_JOGO
+	VALUES
+		('Rod Mario Gonçalves Palhano - Londrina, PR', 'Venda dos Pretos'),
+		('Rua Adalberto Luís Pirola, entre 962-1076 - Estância Bom Tempo Londrina - PR', 'Chácara Boa Esperança'),
+		('Chácara da Marli', NULL),
+		('Rod. Mabio Gonçalves Palhano - Viação Velha, Londrina - PR', 'Campo Viação Velha'),
+		('Rodovia José Garcia de Campos, S/N - Conj. Hab. Jamile Dequech, Londrina - PR, 86044-766', 'Toca do Jacaré'),
+		('Estr. Maj. Archiles Pimpão, 2300 - Jardim Vale Verde, Londrina - PR, 86040-020', 'ACEL'),
+		('R. Vila-Lobos, 885 Jd - Mediterrâneo, Londrina - PR, 86047-130', 'GREPOM'),
+		('R. Vigilato José da Cunha, 578-622 - Jardim dos Alpes I, Londrina - PR, 86075-020', 'Campo Jd. Alpes'),
+		('R. Ver. Valdir Araújo, 158-248, Londrina - PR, 86030-030', 'Chácara Santa Úrsula'),
+		('R. Mauro Feu Figueiras, 3', 'Chácara 3 Figueiras'),
+		('Rua Estrada da Pedreira - Cafezal I Londrina - PR', 'Baixada Santista'),
+		('Rod. Mabio Gonçalves Palhano, 4350 - Gleba Fazenda Palhano, Londrina - PR, 86055-585', 'Chácara do Belinati'),
+		('Rod. Celso Garcia Cid, 1313 - Inglaterra, Londrina - PR, 86046-770', '5o Batalhão da Polícia Militar'),
+		('Chácara do caseiro Amarildo', NULL),
+		('Chácara Dom Bosco', NULL),
+		('R. João-de-Barro, 15 - Indústrias Leves, Londrina - PR, 86030-320', 'Rondopar'),
+		('Rua José Cavalhieri, 07, DATA 08 QUADRA03, Dom Pedro I, Londrina - PR, CEP: 86089-030', 'Retiro dos Cansados'),
+		('Chácara Nissei', NULL),
+		('Recanto das Tulipas', NULL),
+		('Galeria Palhano - s/n lt 4, PR, 86001-970', 'Chácara do Hospital Evangélico'),
+		('Rod. Celso Garcia Cid, 12633 - Columbia, Londrina - PR, 86170-000', 'Kurica Ambiental'),
+		('Campo de Ibiporã', NULL),
+		('Rod. Mabio Gonçalves Palhano, S/n - Ribeirão Gleba Esperança, Londrina - PR, 86057-970', 'APUEL'),
+		('Campo Bratislava - Cambé', NULL),
+		('Sentido saída Curitiba, Rodovia Celso Garcia Cid PR 445, Antes da Cervejaria Spoller.', 'Chácara Toca do Peixe'),
+		('Tv. Ébio Ferraz de Carvalho, 865 - Gleba Lindóia, Londrina - PR, 86031-720', 'ARATEL'),
+		('R. Ernâni Lacerda de Athayde, 1180 - Gleba Fazenda Palhano, Londrina - PR, 86055-630', 'Associação Embrapa');
+
+		
---
+++ Scripts/Script_Migracao_TBL_PONTO_ENCONTRO_v1.sql
@@ -0,0 +1,22 @@
+-- SCRIPT MIGRAÇÃO PONTO DE ENCONTRO
+
+USE CAOFRADES_FC
+
+INSERT INTO TBL_PONTO_ENCONTRO
+	VALUES
+		('Rod. Mabio Gonçalves Palhano, 2170 - Gleba Fazenda Palhano, Londrina - PR, 86055-585', '09:00:00.000', 'Villa 10'),
+		('Av. Chepli Tanus Daher - Jardim Acapulco, Londrina - PR, 86045-020', '09:00:00.000', 'Terminal do Acapulco'),
+		('Av. Salgado Filho, 1334 - Jardim Califórnia, Londrina - PR, 86040-000', '09:00:00.000', 'Praça do Tiro de Guerra'),
+		('Av. Harry Prochet, 369 - Jd Mediterrâneo, Londrina - PR, 86047-040', '09:00:00.000', 'Posto Mediterrâneo'),
+		('Av. Henrique Mansano, 518 - Jardim dos Alpes, Londrina - PR, 86075-000', '09:00:00.000', 'Bar do Ary'),
+		('R. Joubert de Carvalho, 1 - Conj. Vivi Xavier, Londrina - PR, 86082-060', '09:00:00.000', 'Terminal do Vivi Xavier'),
+		('Av. das Maritacas, 1546 - Indusville, Londrina - PR, 86031-070', '09:00:00.000', 'Supermercado 88'),
+		('Av. Eurico Gaspar Dutra - Cafezal I - Londrina - PR, 86049-000', '09:00:00.000', 'Feira livre do Cafezal'),
+		('Rod. Celso Garcia Cid, 1313 - Inglaterra, Londrina - PR, 86046-770', '09:00:00.000', '5o Batalhão da Polícia Militar'),
+		('R. Adalberto Luís Pirola, 86-92 - Sun Lake Residence, Londrina - PR, 86058-162', '09:00:00.000', 'Posto Sun Lake'),
+		('Rod. Celso Garcia Cid, 3570 - Gleba Fazenda Palhano, Londrina - PR, 86044-766', '09:00:00.000', 'Grêmio Esportivo Cacique'),
+		('Av. Guilherme de Almeida, 1271 - Ouro Branco, Londrina - PR, 86042-001', '09:00:00.000', 'Posto Formigão'),
+		('Av. São João, 1217 - Vila Siam, Londrina - PR, 86039-290', '09:00:00.000', 'Posto Alvorada'),
+		('Rod. Melo Peixoto, 5283 - Santa Adelaide, Cambé - PR, 86192-170', '09:00:00.000', 'Churrascaria Botafogo'),
+		('Av. Guilherme de Almeida, 1587 - Prq Ouro Branco, Londrina - PR, 86042-001', '09:00:00.000', 'Posto Ipiranga - Av Guilherme de Almeida'),
+		('Av. Brasília, 5476 - Yara, Londrina - PR, 86027-040', '09:00:00.000', 'Churrascaria Espetos');
