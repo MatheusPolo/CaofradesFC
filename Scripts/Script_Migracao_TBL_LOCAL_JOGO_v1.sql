d10842822886823aef9a19f3e8e20871356a77ef
Matheus <matheuswpolo23@gmail.com>
2020-04-25 19:12:44 -0300

    Migração da TBL_ADVERSARIO e ajustes de base

--- Scripts/Script_Migracao_TBL_LOCAL_JOGO_v1.sql
+++ Scripts/Script_Migracao_TBL_LOCAL_JOGO_v1.sql
@@ -6,7 +6,7 @@ INSERT INTO TBL_LOCAL_JOGO
 	VALUES
 		('Rod Mario Gonçalves Palhano - Londrina, PR', 'Venda dos Pretos'),
 		('Rua Adalberto Luís Pirola, entre 962-1076 - Estância Bom Tempo Londrina - PR', 'Chácara Boa Esperança'),
-		('Chácara da Marli', NULL),
+		(NULL, 'Chácara da Marli'),
 		('Rod. Mabio Gonçalves Palhano - Viação Velha, Londrina - PR', 'Campo Viação Velha'),
 		('Rodovia José Garcia de Campos, S/N - Conj. Hab. Jamile Dequech, Londrina - PR, 86044-766', 'Toca do Jacaré'),
 		('Estr. Maj. Archiles Pimpão, 2300 - Jardim Vale Verde, Londrina - PR, 86040-020', 'ACEL'),
@@ -17,19 +17,20 @@ INSERT INTO TBL_LOCAL_JOGO
 		('Rua Estrada da Pedreira - Cafezal I Londrina - PR', 'Baixada Santista'),
 		('Rod. Mabio Gonçalves Palhano, 4350 - Gleba Fazenda Palhano, Londrina - PR, 86055-585', 'Chácara do Belinati'),
 		('Rod. Celso Garcia Cid, 1313 - Inglaterra, Londrina - PR, 86046-770', '5o Batalhão da Polícia Militar'),
-		('Chácara do caseiro Amarildo', NULL),
-		('Chácara Dom Bosco', NULL),
+		(NULL, 'Chácara do caseiro Amarildo'),
+		(NULL, 'Chácara Dom Bosco'),
 		('R. João-de-Barro, 15 - Indústrias Leves, Londrina - PR, 86030-320', 'Rondopar'),
 		('Rua José Cavalhieri, 07, DATA 08 QUADRA03, Dom Pedro I, Londrina - PR, CEP: 86089-030', 'Retiro dos Cansados'),
-		('Chácara Nissei', NULL),
-		('Recanto das Tulipas', NULL),
+		(NULL, 'Chácara Nissei'),
+		(NULL, 'Recanto das Tulipas'),
 		('Galeria Palhano - s/n lt 4, PR, 86001-970', 'Chácara do Hospital Evangélico'),
 		('Rod. Celso Garcia Cid, 12633 - Columbia, Londrina - PR, 86170-000', 'Kurica Ambiental'),
-		('Campo de Ibiporã', NULL),
+		(NULL, 'Campo de Ibiporã'),
 		('Rod. Mabio Gonçalves Palhano, S/n - Ribeirão Gleba Esperança, Londrina - PR, 86057-970', 'APUEL'),
-		('Campo Bratislava - Cambé', NULL),
+		(NULL, 'Campo Bratislava - Cambé'),
 		('Sentido saída Curitiba, Rodovia Celso Garcia Cid PR 445, Antes da Cervejaria Spoller.', 'Chácara Toca do Peixe'),
 		('Tv. Ébio Ferraz de Carvalho, 865 - Gleba Lindóia, Londrina - PR, 86031-720', 'ARATEL'),
 		('R. Ernâni Lacerda de Athayde, 1180 - Gleba Fazenda Palhano, Londrina - PR, 86055-630', 'Associação Embrapa');
+
 
 		
