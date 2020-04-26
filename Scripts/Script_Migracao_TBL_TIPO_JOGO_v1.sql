716453580d367fd3f5e5db2cf3d85a7f79388061
Matheus <matheuswpolo23@gmail.com>
2020-04-26 16:21:12 -0300

    Inicio migração TBL_JOGOS e alterações de tabelas

---
+++ Scripts/Script_Migracao_TBL_TIPO_JOGO_v1.sql
@@ -0,0 +1,7 @@
+-- SCRIPT MIGRAÇÃO TIPO JOGO
+
+INSERT INTO TBL_TIPO_JOGO
+	VALUES
+		('Amistoso'),
+		('1a Copa União Acabamentos/MS Calçados de Futebol Suiço Livre'),
+		('2a Copa União Acabamentos/MS Calçados de Futebol Suiço Livre');
---
+++ Scripts/Script_Migracao_TBL_UNIFORME_v1.sql
@@ -0,0 +1,8 @@
+-- SCRIPT MIGRAÇÃO UNIFORMES
+
+USE CAOFRADES_FC
+
+INSERT INTO TBL_UNIFORME
+	VALUES
+		('Uniforme 1'),
+		('Uniforme 2');
